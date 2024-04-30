c5c13e423d8a43a44e2e2bcea3f459693da6a01f
kirankumarkommu007 <kommukirankumar007@gmail.com>
2024-05-01 02:15:32 +0530

    added react for okta

--- src/App.js
+++ src/App.js
@@ -1,23 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import { useAuth0 } from '@auth0/auth0-react';
+import axios from 'axios';
+
+
 
 function App() {
+  const { isLoading, isAuthenticated, user,getAccessTokenSilently,  loginWithRedirect, logout } = useAuth0();
+  const [apiResponse , setApiResponse] = useState(null);
+
+   
+  const fetchPublicAPI = async () => {
+    try {
+      
+      const response = await axios.get("http://localhost:3001/api/public");
+      console.log(response.data.message)
+      setApiResponse(response.data.message);
+    } catch (error) {
+      console.error('Failed to fetch public API:', error);
+      setApiResponse('Error fetching public API'); 
+    }
+  };
+
+  const fetchProtectedAPI=async()=>{
+    try {
+      const accessToken=await getAccessTokenSilently();
+      const response = await axios.get('http://localhost:3001/api/private', {
+          headers: {
+            Authorization: `Bearer ${accessToken}`
+          }
+        });
+      setApiResponse(response.data.message);
+      console.log(response.data.message,+" "+accessToken)
+      }
+    catch (error) {
+      console.error('Failed to fetch public API:', error);
+      setApiResponse('Error fetching protected API'); 
+      
+    }
+  };
+
+  const fetchScopedAPI=async()=>{
+    try {
+      const accessToken = await getAccessTokenSilently({
+        scope: 'read:resource' 
+      });
+      const response = await axios.get('http://localhost:3001/api/private-scoped', {
+          headers: {
+            Authorization: `Bearer ${accessToken}`
+          }
+        });
+      setApiResponse(response.data.message);
+      console.log(response.data.message,+" "+accessToken)
+      }
+    catch (error) {
+      console.error('Failed to fetch public API:', error);
+      setApiResponse('Error fetching protected API'); 
+      
+    }
+  };
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {!isAuthenticated && (
+        <button onClick={loginWithRedirect}>Log in</button>
+      )}
+
+      {isAuthenticated && (
+        <>
+          <p>Hello, {user.name}</p>
+          <button onClick={() => logout({ returnTo: window.location.origin })}>Log out</button>
+          <button onClick={fetchProtectedAPI} >Fetch Private API</button>
+          <button onClick={fetchPublicAPI}>Fetch Public API</button>
+          <button onClick={fetchScopedAPI}>Fetch Scoped API</button>
+          
+          {apiResponse ? (
+            <p>Response Message: {apiResponse}</p>
+          ) : (
+            <p>No data fetched yet</p>
+          )}
+        </>
+      )}
     </div>
   );
 }
